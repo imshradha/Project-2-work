ff3383d25eb9c7c9b0dc044517482283c00b71f1
imshradha <shraddha824swan@gmail.com>
2022-05-05 10:59:18 +0530

    added comments

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,16 +1,17 @@
-const internModel = require("../models/internModel");    // import internModel
-const collegeModel = require("../models/collegeModel");  //Import collegeModel
-// const { findOne } = require("../models/collegeModel");
+const internModel = require("../models/internModel");// import internModel
+const collegeModel = require("../models/collegeModel");//Import collegeModel
 
+//create intern function
 const createIntern = async function (req, res) {
     try {
+        //Reading input from req.body
         const requestBody = req.body;
 
         const errors = await validateIntern(requestBody);
         if (errors.length > 0) {
             return res.status(400).send({ status: false, msg: "Mandatory fields are missing", errors: errors });
         }
-
+        //assigning values to variables
         const { name, email, mobile, collegeName } = requestBody;
 
         //check mobile number is valid or not
@@ -58,12 +59,13 @@ const createIntern = async function (req, res) {
     catch (error) {
         return res.status(500).send({ status: false, msg: error.message })
     }
-
 }
 
 const validateIntern = async function (internData) {
     const errors = [];
+    //assigning values to multiple variables
     const { name, email, mobile, collegeName } = internData   //destructuring internData object
+
     //Mandotory fields
     if (!name || name.trim().length === 0) {
         errors.push("name required")
@@ -80,10 +82,13 @@ const validateIntern = async function (internData) {
     return errors;
 }
 
-
+//get college details for the requested college
 const getCollegeDetails = async function (req, res) {
     try{
+        //Reading input from query param
         const data = req.query;
+
+        //Reading collegename from query param
         const collegeName = data.collegeName;
 
         if (!collegeName || collegeName.trim().length === 0) {
@@ -96,6 +101,7 @@ const getCollegeDetails = async function (req, res) {
         if (!collegeDetails) {
             return res.status(404).send({ status: false, message: `${collegeName} college not found`})
         }
+        //assigning values to multiple variables
         const { name, fullName, logoLink, _id } = collegeDetails; // destructuring
 
         //find documents for interests in given college using _id
@@ -110,5 +116,6 @@ const getCollegeDetails = async function (req, res) {
     }
 }
 
+//export function
 module.exports.createIntern = createIntern;
 module.exports.getCollegeDetails = getCollegeDetails;
--- src/route/route.js
+++ src/route/route.js
@@ -7,10 +7,11 @@ const internContoller = require('../controllers/internController');
 const app = express()//used express to create global middleware
 
 //api's
+//create college api
 router.post('/functionup/colleges', collegeContoller.createCollege);
-
+//create intern api
 router.post('/functionup/interns', internContoller.createIntern);
-
+//get college api
 router.get('/functionup/collegeDetails', internContoller.getCollegeDetails)
 
 //export router
